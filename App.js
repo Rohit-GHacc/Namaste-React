62e72a41bcb61846c3d621406b04f1107b4a949a
Rohit-GHacc <rohitchugh142001@gmail.com>
2025-02-22 01:54:14 +0530

    Created Nested Elements structure without using jsx

---
+++ App.js
@@ -0,0 +1,20 @@
+
+{/* 
+    <div id ="parent">
+        <div id = "child">
+            <h1>Hello Rohit learn react quickly</h1>
+            <h1> Ok Boss !!</h1>   
+        </div>
+    </div> 
+*/}
+
+
+const heading1 = React.createElement('h1', {key:'heading1'}, "Rohit! Learn React quickly!")
+const heading2 = React.createElement('h2', {key:'heading2'}, "Ok Boss !!")
+const child = React.createElement('div',{id: "child"},[heading1, heading2])
+const parent = React.createElement('div',{id:"parent",},child)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent);
+
+
