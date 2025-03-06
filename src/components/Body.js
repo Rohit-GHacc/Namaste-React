ec37454e8c5cadafd4ed52ae44d5f4580998c3a8
Rohit-GHacc <rohitchugh142001@gmail.com>
2025-03-06 15:44:55 +0530

    1.) Fetched real data from swiggy api and removed mock data, 2.) Added Shimmer UI, till the data from api is fetched Shimmer UI is shown to the user for better User Experience : )

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import { resList } from "../utils/mockData";
-const Body = ()=>{
-    const [filteredList, setFilteredList] = useState(resList);
-    const search = ()=>{
-        setFilteredList(resList.filter((restaurant)=>restaurant.info.avgRating > 4.4));
+import Shimmer from './Shimmer'
+const Body = () => {
+    const [load, setLoad] = useState(true)
+    const [filteredList, setFilteredList] = useState([]);
+    useEffect(() => {
+        fetchData();
+    }, [load])
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.99308229999999&lng=77.0150735&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        const json = await data.json();
+        console.log(json);
+        setFilteredList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     }
-    const showAll = ()=>{
-        setFilteredList(resList);
+    const showTopRated = () => {
+        setFilteredList(filteredList.filter((restaurant) => restaurant.info.avgRating > 4.4));
     }
-    return <div>
-        <div className="search">
-            <button className = 'search-btn' onClick={search}> Top Rated Restaurants</button>
-            <button className = 'search-btn' onClick={showAll}> Show All Restaurants</button>
+    const showAll = () => {
+        setLoad((prev) => !prev)
+    }
+    console.log(filteredList.length)
+    if (filteredList.length === 0) {
+        return <Shimmer />
+    }
+    return (
+        <div>
+            <div className="search">
+                <button className='search-btn' onClick={showTopRated}> Top Rated Restaurants</button>
+                <button className='search-btn' onClick={showAll}> Show All Restaurants</button>
+            </div>
+            <div className='res-card-container'>
+                {
+                    filteredList.map((restaurant) => (
+                        <RestaurantCard key={restaurant.info.id} resData={restaurant.info} />
+                    ))
+                }
+            </div>
         </div>
-        <div className = 'res-card-container'>
-            {
-                filteredList.map((restaurant)=>(
-                    <RestaurantCard key = {restaurant.info.id} resData = {restaurant.info}/>
-                ))
-            }
-        </div>
-    </div>
+    )
 }
 export default Body;
---
+++ src/components/Shimmer.js
@@ -0,0 +1,20 @@
+const Shimmer = ()=>{
+    return  <div className = 'shimmer-card-container'>
+                <div className = 'shimmer-card'> <div className='dark-bg'></div> </div>
+                <div className = 'shimmer-card'> <div className='dark-bg'></div> </div>
+                <div className = 'shimmer-card'> <div className='dark-bg'></div> </div>
+                <div className = 'shimmer-card'> <div className='dark-bg'></div> </div>
+                <div className = 'shimmer-card'> <div className='dark-bg'></div> </div>
+                <div className = 'shimmer-card'> <div className='dark-bg'></div> </div>
+                <div className = 'shimmer-card'> <div className='dark-bg'></div> </div>
+                <div className = 'shimmer-card'> <div className='dark-bg'></div> </div>
+                <div className = 'shimmer-card'> <div className='dark-bg'></div> </div>
+                <div className = 'shimmer-card'> <div className='dark-bg'></div> </div>
+                <div className = 'shimmer-card'> <div className='dark-bg'></div> </div>
+                <div className = 'shimmer-card'> <div className='dark-bg'></div> </div>
+                <div className = 'shimmer-card'> <div className='dark-bg'></div> </div>
+                <div className = 'shimmer-card'> <div className='dark-bg'></div> </div>
+                <div className = 'shimmer-card'> <div className='dark-bg'></div> </div>
+            </div>
+}
+export default Shimmer
