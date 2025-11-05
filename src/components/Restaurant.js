17f6e87f880463bf6b60ac0febe38fd303fb5e8d
Rohit-GHacc <rohitchugh142001@gmail.com>
2025-11-06 00:01:54 +0530

    just trying to open the ResCard, not working probably because trying at late night when swiggy's shut down

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,24 +1,39 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom';
+import Shimmer from './Shimmer';
 const Restaurant = ()=>{
+    const [items,setItems] = useState(null);
+    const [resInfo,setResInfo] = useState(null);
+    const {id} = useParams();
     useEffect(()=>{
-        // const data = await fetch();
-        // const json = await data.json()
-        const fetch=  async()=>{
-            console.log("abdcd")
-        }
-        fetch();
+        fetchMenu();
     },[])
+    const fetchMenu =  async()=>{
+        // console.log("abdcd")
+        const data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.99308229999999&lng=77.0150735&restaurantId=${id}&catalog_qa=undefined`);
+        const json = await data.json();
+        console.log(json);
+        setResInfo(json);
+        const itemList = json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[4]?.card?.card?.itemCards;
+        console.log(itemList);
+        setItems(itemList);
+    }
+    if(resInfo===null) return <Shimmer/>
+    const {name,areaName}  = resInfo?.data?.cards[2]?.card?.card?.info;
+
     return (
         <div>
-            <h1>Name of the Restaurant</h1>
-            <h2> Locality</h2>
+            <h1>{name}</h1>
+            <h2> {areaName}</h2>
             <h2> Dishes</h2>
             <ul>
-                <li>Dish 1</li>
-                <li>Dish 1</li>
-                <li>Dish 1</li>
-                <li>Dish 1</li>
-            </ul>
+            {
+                items.map((item)=>{
+                    return (
+                        <li>{item?.card?.info?.name}</li>
+                    )
+                })
+            } </ul>
         </div>
     )
 }
