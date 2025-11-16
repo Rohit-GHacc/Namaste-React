c6d4d61bc645e7f1fd4cf9f8576034ecb4d46431
Rohit-GHacc <rohitchugh142001@gmail.com>
2025-11-16 13:41:46 +0530

    Added custom hook useOnillneStatus

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import {LOGO_URL}  from "../utils/constant";
 import { Link } from "react-router-dom";
-const Header = ()=>(
+import useOnlineStatus from '../utils/useOnlineStatus'
+const Header = ()=>{
+    const isOnline = useOnlineStatus();
+    return (
     <div className = 'nav'>
         <img src= {LOGO_URL} className = 'logo'/>
         <ul className = 'nav-list'>
+            <li>Online: {isOnline ? '🟢' : '🔴'}</li>
             <Link to="/"><li>Home</li></Link>
             <Link to="/about"><li>About Us</li></Link>
             <Link to="/contact"><li>Contact Us</li></Link>
             <Link to="/cart"><li>Cart</li></Link>
         </ul>
     </div>
-)
+    )
+}
 export default Header;
