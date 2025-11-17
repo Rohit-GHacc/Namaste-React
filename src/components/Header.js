3129c55a01ec5e93f2ba869ca42e6b9a5159e3a1
Rohit-GHacc <rohitchugh142001@gmail.com>
2025-11-17 17:23:30 +0530

    Added tailwind css and styled header component using it

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import useOnlineStatus from '../utils/useOnlineStatus'
 const Header = ()=>{
     const isOnline = useOnlineStatus();
     return (
-    <div className = 'nav'>
-        <img src= {LOGO_URL} className = 'logo'/>
-        <ul className = 'nav-list'>
+    <div className = 'flex justify-between shadow-lg m-2 p- bg-pink-100'>
+        <img src= {LOGO_URL} className = 'w-50'/>
+        <ul className = 'flex items-center text-2xl space-x-8 px-4'>
             <li>Online: {isOnline ? '🟢' : '🔴'}</li>
             <Link to="/"><li>Home</li></Link>
             <Link to="/about"><li>About Us</li></Link>
